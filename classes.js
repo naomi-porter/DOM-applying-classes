3f922a27e7445af04c7bacad0082ea16bd9cd975
Naomi Porter <nomyis@hotmail.com>
2020-12-02 16:27:44 +1300

    Sprint 3, Dom challenges, Classes.js completed 1-6

--- classes.js
+++ classes.js
@@ -5,10 +5,13 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+  one()  
+  two()  
+  three()
+  
   // Your turn! Create a new function called `two`, then call it from here.
-}
+} 
+
 
 function one () {
   // First, we have to find the element:
@@ -19,7 +22,17 @@ function one () {
 }
 
 // CREATE FUNCTION two HERE
-
+function two () {
+  var two = document.getElementById('two')
+  two.classList.add('green')
+}
 // CREATE FUNCTION three HERE
-
+function three () {
+  var three = document.getElementById('three')
+  three.classList.add('orchid')
+}
 // CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+  var ? = document.getElementsByClassName('invisible');
+  invisible.classList.add('visible')
+}
